21db10beec7e11118b9d0ca011b7f0c828dbfa94
Sajid <mdsajidali1803@gmail.com>
2024-04-30 23:55:11 +0530

    Add data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version.
+
+// alert(3 + 3) // Not allowed coz we are using Nodejs, not browser.
+
+let name = "Sajid"
+let age =  21
+let isLoggedIn = false
+
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => Value is not assigned
+// symbol => for uniqueness 
+
+// object => We will discuss in future.
+
+console.log(typeof "Sajid"); // o/p: string
+console.log(typeof age);    // o/p: number
+console.log(typeof null);  //  o/p: object
+console.log(typeof undefined); // o/p: undefined
