e316b584b003e181b3cca178ac9ad22a80492d66
Sajid <mdsajidali1803@gmail.com>
2024-05-04 23:50:57 +0530

    Add functions basics

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -88,3 +88,71 @@ function signUpUserMessage(username = "Siri") {
 
 console.log(signUpUserMessage());
 console.log(signUpUserMessage("Sam"));
+
+console.log("\n");
+
+
+
+
+//
+function calculateCartPriceOfAmazon(price){
+    return price
+}
+
+console.log(calculateCartPriceOfAmazon(99));
+console.log(calculateCartPriceOfAmazon(199, 299, 399));
+
+console.log("\n");
+
+
+//
+function calculateCartPriceOfFlipkart(...price){  // ...price is a rest parameter
+    return price
+}
+
+console.log(calculateCartPriceOfFlipkart(99));
+console.log(calculateCartPriceOfFlipkart(199, 299, 399));
+
+console.log("\n");
+
+
+//
+function calculateCartPriceOfMyntra(value1, value2, ...price) { // A rest parameter(...price) must be last in a parameter list.
+    return price
+}
+
+console.log(calculateCartPriceOfMyntra(499, 799, 1049, 1199, 1699));
+
+console.log("\n");
+
+
+
+//
+const user = {
+    username: "Jacks",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
+
+handleObject({
+    username: "Max",
+    price: 399
+})
+
+
+
+
+//
+const myNewArray = [100, 200, 300, 400]
+
+function returnSecondValue(getArrayyyyyyy){
+    return getArrayyyyyyy[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([100, 200, 300, 400]));
