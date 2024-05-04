9c019ca5376e6be1b48e866aa3798c9b08056445
Sajid <mdsajidali1803@gmail.com>
2024-05-05 01:10:20 +0530

    Add scopes basics

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -27,3 +27,67 @@ if(true) {
 console.log(x);
 console.log(y);
 console.log(z);
+
+console.log("\n");
+
+
+
+// Multiple scope //
+
+function one() {
+    const username = "Sajid"
+
+    function two() {
+        const website = "Youtube"
+        console.log(username); // We can access the 'username' because two() function is inside the one() function.
+        console.log(website);
+    }
+    //console.log(website); // We can not access the 'website' because the scope of 'website' is limited only within the two() function.
+
+    two()
+}
+
+one()
+
+console.log("\n");
+
+
+//
+if(true) {
+    const username = "MAxwell"
+    if(username === "MAxwell") {
+        const website = "Google"
+        console.log(username + website);
+        console.log(website);
+    }
+    //console.log(website); // We can not access the 'website' because the scope of 'website' is limited only within the two() function.
+    console.log(username);
+}
+
+//console.log(username); // We can not access the 'username' because the scope of 'username' is limited only within the if(){}.
+
+console.log("\n");
+
+
+// *************************** Interesting ********************** //
+
+//
+console.log("Hey", addOne(5));
+
+function addOne(num) {
+    return num + 1
+}
+
+console.log("Hello", addOne(7));
+
+console.log("\n");
+
+
+//
+//console.log("Hi", addTwo(5)); // We can not do this in this case.
+
+const addTwo = function(num) {
+    return num + 2
+}
+
+console.log("Ok", addTwo(9));
