d03cef731474f649c1bc6ccfed358e9016ec0c3d
Sajid <mdsajidali1803@gmail.com>
2024-05-13 01:01:20 +0530

    Add while and  do-while loops

---
+++ 06_iterations/02_second.js
@@ -0,0 +1,35 @@
+
+//
+let index = 0
+while(index <= 10){
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+console.log("\n");
+
+//
+let myArray = ['flash', 'batman', 'superman']
+let arr = 0
+while(arr < myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+console.log("\n");
+
+
+//
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 5);
+
+console.log();
+
+let score1 = 6
+do {
+    console.log(`Score is ${score1}`);
+    score++
+} while (score <= 5);
