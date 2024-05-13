5c63219147375131e96a302152628ae3f4665dcc
Sajid <mdsajidali1803@gmail.com>
2024-05-14 01:28:26 +0530

    Add .reduce

---
+++ 06_iterations/08_eighth.js
@@ -0,0 +1,48 @@
+//
+const array1 = [1, 2, 3, 4]
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+    (accumulator, currentValue) => accumulator + currentValue, initialValue
+);
+
+console.log(sumWithInitial);
+// Expected output: 10 (0 + 1 + 2 + 3 + 4)
+
+
+console.log("\n");
+
+
+//
+const myNums = [1, 2, 3, 4]
+const myTotal = myNums.reduce(
+    (accumulator, currentValue) => accumulator + currentValue, 0
+);
+
+console.log(sumWithInitial);
+// Expected output: 10 (0 + 1 + 2 + 3 + 4)
+
+console.log("\n");
+
+
+
+//
+const shoppingCart = [
+
+    {
+        itemName: "Shoes",
+        price: 2999
+    },
+    {
+        itemName: "Jeans",
+        price: 1999
+    },
+    {
+        itemName: "Shirt",
+        price: 999
+    }
+
+]
+
+const priceToPay = shoppingCart.reduce( (accumulator, item) => accumulator + item.price,  0)
+
+console.log(priceToPay);
